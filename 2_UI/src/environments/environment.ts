de738d6e0041c751bbda6305425a86947b54568a
shruthisathish <shruthi.devesh@gmail.com>
2021-10-28 05:11:45 +0000

    Big Data HW4 Commit 1

--- 2_UI/src/environments/environment.ts
+++ 2_UI/src/environments/environment.ts
@@ -8,15 +8,15 @@ export const environment = {
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: '',
+  cognitoIdentityPoolId: 'eu-west-1:bf02d34e-db40-4d71-a62d-0b76888330c9',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
-  facebookAppId: '',
+  facebookAppId: '1039301350239656',
 
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: ''
+  ticketAPI: 'https://api.devsoftech.com/'
 
 };
